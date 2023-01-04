d9f2ae03d618bb0354d8d1b397242f2d4719f449
Chromatic-Chaos <116543872+Chromatic-Chaos@users.noreply.github.com>
2023-01-04 10:15:39 +0100

    bravo

---
+++ karakalk/js/index.js
@@ -0,0 +1,21 @@
+let resultat = 100
+
+
+if(resultat >= 90){
+    "du fikk karakterene 6!"
+}
+else if(resultat >= 80){
+    "du fikk karakterene 5!"
+}
+else if(resultat >= 70){
+    "du fikk karakterene 4!"
+}
+else if(resultat >= 60){
+    "du fikk karakterene 3!"
+}
+else if(resultat >= 50){
+    "du fikk karakterene 2!"
+}
+else if(resultat < 50){
+    "du besto ikke..."
+}

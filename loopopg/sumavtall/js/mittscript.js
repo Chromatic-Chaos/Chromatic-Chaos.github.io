2d9dcfda21553606be6d7f9096e7a54206fe1f21
Chromatic-Chaos <116543872+Chromatic-Chaos@users.noreply.github.com>
2023-01-09 08:50:23 +0100

    delta

---
+++ loopopg/sumavtall/js/mittscript.js
@@ -0,0 +1,11 @@
+let tall = [1, 2, 3, 4, 5]
+
+
+
+function sum(tall) {
+    let total = 0;
+    for (let i = 0; i < tall.length; i++){
+        return total;
+    }
+}
+console.log(sum([1, 2, 3, 4, 5, 5]))

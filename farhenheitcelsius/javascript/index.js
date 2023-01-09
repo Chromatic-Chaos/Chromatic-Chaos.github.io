9d95f2bb277e5475e3338f78174e06c3af1022d6
Chromatic-Chaos <116543872+Chromatic-Chaos@users.noreply.github.com>
2023-01-09 08:30:28 +0100

    charlie

---
+++ farhenheitcelsius/javascript/index.js
@@ -0,0 +1,11 @@
+
+
+
+function tilcelsius(){
+
+}
+const fahrenheit = prompt("vennligst tast inn en Fahrenheit verdi: ")
+
+const celsius = (fahrenheit - 32) / 1.8;
+
+console.log(`${celsius} grader celsius er ${fahrenheit} grader fahrenheit)
---
+++ loopopg/7gangen/.js/script.js
@@ -0,0 +1,8 @@
+for (i=0; i<= 70; i += 7){
+    console.log(i)
+}
+liste=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (i =0; i < liste.length; i++){
+    tall=liste[i]*7;
+    console.log(tall);
+}

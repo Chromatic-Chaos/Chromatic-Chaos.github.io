455c1d0b0feb7715aa0c35131f41d91db3d5a748
Chromatic-Chaos <116543872+Chromatic-Chaos@users.noreply.github.com>
2023-01-11 21:54:35 +0100

    alfafoxtrot

--- loopopg/5gangen/js/script.js
+++ loopopg/5gangen/js/script.js
@@ -1,18 +1,49 @@
 function gangmed(){
+        tal1=
+        document.getElementById("tall1").value;
+        tal2=
+        document.getElementById("tall2").value;
+    
+        document.getElementById("resultat").innerHTML = tal1*tal2;
+        }
+function delmed(){
     tal1=
-    document.getElementById("tall").value;
-    
-    tal2=[5]
+    document.getElementById("tall1").value;        
+    tal2=
+    document.getElementById("tall2").value;
+    document.getElementById("resultat").innerHTML = tal1/tal2;
+}
+function minus(){
+    tal1=
+    document.getElementById("tall1").value;        
+    tal2=
+    document.getElementById("tall2").value;
+    document.getElementById("resultat").innerHTML = tal1-tal2;
+}
 
-    document.getElementById("resultat").innerHTML = tal1*tal2;
-    }
-    
+isNaN()
 
 
+//Det som er under er mislykkete forsøk på å få addering til å fungere. Jeg har ingen aning hvorfor det ikke fungerer
 
+
+function minus(){
+    tal1=
+    document.getElementById("tall1").value;        
+    tal2=
+    document.getElementById("tall2").value;
+    document.getElementById("resultat").innerHTML = tal1+tal2;
+}
+
+function plus(a,b){ 
+    a=document.getElementById("tall1").value;
+    b=document.getElementById("tall2").value;
+    return a + b ; document.getElementById("resultat").innerHTML
+}
+/*
     liste=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
     for (i =0; i < liste.length; i++){
         tall=liste[i]*5;
         console.log(tall);
-}
+}*/

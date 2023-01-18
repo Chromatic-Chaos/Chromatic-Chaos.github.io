068652728131244bd4c2bb2a655cdb8d5624cbf2
Chromatic-Chaos <116543872+Chromatic-Chaos@users.noreply.github.com>
2023-01-18 11:00:04 +0100

    alfagolf

---
+++ function/js/mittscript.js
@@ -0,0 +1,9 @@
+const aNum=5
+
+function addNumbers(someNumber){
+    return someNumber + aNum
+}
+
+const someNumber= 10
+
+console.log(addNumbers(someNumber))
--- loopopg/5gangen/js/script.js
+++ loopopg/5gangen/js/script.js
@@ -13,7 +13,13 @@ function delmed(){
     document.getElementById("tall2").value;
     document.getElementById("resultat").innerHTML = tal1/tal2;
 }
-function minus(){
+function plus(){
+    tal1 = document.getElementById("tall1").value;
+    tal2 = document.getElementById("tall2").value;
+    sum = tal1 + tal2;
+    document.write("Resultatet er:"+ sum);
+}
+function plus(){
     tal1=
     document.getElementById("tall1").value;        
     tal2=
@@ -21,13 +27,15 @@ function minus(){
     document.getElementById("resultat").innerHTML = tal1-tal2;
 }
 
-isNaN()
+isNaN("En av variablene er ikke et tall, skriv inn et tall.")
 
 
 //Det som er under er mislykkete forsøk på å få addering til å fungere. Jeg har ingen aning hvorfor det ikke fungerer
 
 
-function minus(){
+
+
+/*function plus(){
     tal1=
     document.getElementById("tall1").value;        
     tal2=
@@ -38,7 +46,7 @@ function minus(){
 function plus(a,b){ 
     a=document.getElementById("tall1").value;
     b=document.getElementById("tall2").value;
-    return a + b ; document.getElementById("resultat").innerHTML
+    
 }
 /*
     liste=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
